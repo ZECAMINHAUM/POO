02e44fd5889ee9969ca427fcb137c9137932224c
LUCAS SILVA <ls4388387@gmail.com>
2018-03-13 12:40:34 -0300

    agora sim

--- Geometricas/src/geometricas/Principal.java
+++ Geometricas/src/geometricas/Principal.java
@@ -21,29 +21,30 @@ public class Principal extends JFrame{
     
     public Principal(){
         
-        setTitle("Formas Geometricas");
-        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-        
+       
         //Criando
         bt_circulo = new JButton("Circulo");
         bt_quadrado = new JButton("Quadrado");
         bt_retangulo = new JButton("Retangulo");
         bt_triangulo = new JButton("Triangulo");
-        
+         
         //adicionando
         getContentPane().add(bt_circulo);
         getContentPane().add(bt_quadrado);
         getContentPane().add(bt_retangulo);
         getContentPane().add(bt_triangulo);
         
-        
-    
+      
         
     }
     
     public static void main (String[] args){
         
-        new Principal().setVisible(true);
+        Principal p = new Principal();
+        p.setVisible(true);
+        p.setTitle("Formas Geometricas");
+        p.setDefaultCloseOperation(p.EXIT_ON_CLOSE);
+        p.setSize(500, 400);
         
     }
     
